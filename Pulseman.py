be3fd75952bd7898ac64007ff4b52ac89270526c
HAMSATH-SAIT Abdul-Rahman <abdul-rahman.hamsath-sait-extern@rntbci.com>
2023-11-13 14:50:30 +0530

    C11

--- Pulseman.py
+++ Pulseman.py
@@ -13,9 +13,9 @@ ser = serial.Serial ("/dev/ttyS0", 9600)
 # Loop
 while(1):
     received_data = ser.read() 
-    time.sleep(0.03)
+    time.sleep(0.1)
     data_left = ser.inWaiting()
     received_data += ser.read(data_left)
     print(received_data.decode("utf-8"))
-    # writeToLCD(lcd, received_data, 1)
+    writeToLCD(lcd, received_data, 1)
     #time.sleep(5)
