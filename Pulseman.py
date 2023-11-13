e7f80ff7fbd925d75235c012bfd4612d2c67dc67
HAMSATH-SAIT Abdul-Rahman <abdul-rahman.hamsath-sait-extern@rntbci.com>
2023-11-13 19:10:15 +0530

    C32

--- Pulseman.py
+++ Pulseman.py
@@ -3,6 +3,7 @@ from rpi_lcd import LCD
 import time
 import serial
 import json
+from adafruit_servokit import ServoKit
 
 # Comp imports
 from LCDUtil import *
@@ -10,6 +11,9 @@ from LCDUtil import *
 # Inits
 lcd = LCD()
 ser = serial.Serial ("/dev/ttyS0", 9600)
+kit = ServoKit(channels=16)
+
+# Glovars
 received_data = bytearray()
 newData = False
 
@@ -23,9 +27,14 @@ while(1):
         # print(received_data) 
         try:
             data = json.loads(received_data.decode("utf-8")) 
+            print("Command Recieved:")
             print(json.dumps(data, indent=4)) 
+            print("")
             clearLCD(lcd) 
-            writeToLCD(lcd, "Recieved " + data["type"] + " cmd", 1)
+            writeToLCD(lcd, "cmd>" + data["type"], 1)
+            writeToLCD(lcd, "writing to PCA", 2)
+            kit.servo[0].angle = data["value"]["A"]["angle"]
+            kit.servo[1].angle = data["value"]["B"]["angle"]
             ser.write("Aye Matey!\n\r".encode("utf-8"))
         except:
             ser.write("Nay Matey!\n\r".encode("utf-8"))
