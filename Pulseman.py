47cce0e4a8f03f66629f89f41243228683a9c26a
HAMSATH-SAIT Abdul-Rahman <abdul-rahman.hamsath-sait-extern@rntbci.com>
2023-11-13 15:06:02 +0530

    C15

--- Pulseman.py
+++ Pulseman.py
@@ -9,12 +9,19 @@ from LCDUtil import *
 # Inits
 lcd = LCD()
 ser = serial.Serial ("/dev/ttyS0", 9600)
+print(serial.__version__)
+received_data = 0
+newData = false
 
 # Loop
 while(1):
     while ser.in_waiting: 
-        received_data = ser.read() 
-        print(received_data.decode("utf-8"), end='')
+        received_data += ser.read() 
+        newData = true
+        
+    if newData:    
+        print(received_data.decode("utf-8"))    
         writeToLCD(lcd, received_data.decode("utf-8"), 1)
-    
+        newData = false
+        
     time.sleep(5)
